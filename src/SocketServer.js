740fbb82d11e41e7f843192b2fb366344ce4184c
Aditya Nugraha <nugrahaa009@gmail.com>
2023-10-08 03:21:05 +0700

    online status

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,8 +1,24 @@
-export default function (socket) {
+let onlineUsers = []
+
+export default function (socket, io) {
   //user joins or opens the application
   socket.on("join", (user) => {
     socket.join(user);
+    //add joined user to online users
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      onlineUsers.push({ userId: user, socketId: socket.id })
+    }
+    //send online users to frontend
+    io.emit("get-online-users", onlineUsers)
   });
+
+  //socket disconnect
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("get-online-users", onlineUsers)
+  })
+
+  //socket offline
 
   //join a conversation room
   socket.on("join conversation", (conversation) => {
